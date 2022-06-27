cde61a8647d6a47c8151c89f33473f315c7056a8
DaniloVoid <danilovsdanilo@gmail.com>
2022-06-27 17:25:11 -0400

    finish form registrar viagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 // components local
 import { ViagemComponent } from './viagem/viagem.component';
@@ -17,6 +18,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
@@ -34,9 +38,12 @@ import { MatSelectModule } from '@angular/material/select';
     MatCardModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatRadioModule,
+    MatDividerModule,
 
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/viagem/viagem.component.ts
+++ src/app/viagem/viagem.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpService, Country, City } from '../services/http.service';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-viagem',
   templateUrl: './viagem.component.html',
@@ -13,6 +13,11 @@ export class ViagemComponent implements OnInit {
   countriesDestiny: Country[] = []
   citiesOrigin?: City[] = []
   citiesDestiny?: City[] = []
+  qtdAdults: number = 0
+  qtdchildren: number = 0
+  form = new FormGroup({
+    miles: new FormControl(0)
+  })
 
   private _countrySelectOrigin?: Country
   private _countrySelectDestiny?: Country
@@ -35,6 +40,20 @@ export class ViagemComponent implements OnInit {
   }
   set countrySelectDestiny(country) {
     this.citiesDestiny = this.countriesDestiny.find(c => c === country)?.cities
+  }
+
+  incrementQtdAdults() {
+    this.qtdAdults++
+  }
+  decrementQtdAdults() {
+    if (this.qtdAdults > 0) this.qtdAdults--
+  }
+
+  incrementQtdchildren() {
+    this.qtdchildren++
+  }
+  decrementQtdchildren() {
+    if (this.qtdchildren > 0) this.qtdchildren--
   }
 
 }
